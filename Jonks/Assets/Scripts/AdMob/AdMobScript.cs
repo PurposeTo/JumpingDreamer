c2ff4a163133768cfefcdbcc7798eb036a0f4c05
PurposeTo <firecrystal1999@gmail.com>
2020-07-06 19:54:14 +0300

    Added Ad testing scene
    Added Ad testing scene with rewarded video ad

---
+++ Jonks/Assets/Scripts/AdMob/AdMobScript.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdMobScript : MonoBehaviour
+{
+    private const string ADS_ID = "ca-app-pub-8365272256827287~9135876659";
+
+    private const string rewardedVideoAd_ID = "ca-app-pub-8365272256827287/5171106131";
+
+    private RewardBasedVideoAd rewardBasedVideoAd;
+
+
+    private void Start()
+    {
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(initStatus => { });
+
+        // Get singleton reward based video ad reference.
+        this.rewardBasedVideoAd = RewardBasedVideoAd.Instance;
+
+        // Called when an ad request has successfully loaded.
+        rewardBasedVideoAd.OnAdLoaded += HandleRewardBasedVideoLoaded;
+        // Called when an ad request failed to load.
+        rewardBasedVideoAd.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
+        // Called when an ad is shown.
+        rewardBasedVideoAd.OnAdOpening += HandleRewardBasedVideoOpened;
+        // Called when the ad starts to play.
+        rewardBasedVideoAd.OnAdStarted += HandleRewardBasedVideoStarted;
+        // Called when the user should be rewarded for watching a video.
+        rewardBasedVideoAd.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        // Called when the ad is closed.
+        rewardBasedVideoAd.OnAdClosed += HandleRewardBasedVideoClosed;
+        // Called when the ad click caused the user to leave the application.
+        rewardBasedVideoAd.OnAdLeavingApplication += HandleRewardBasedVideoLeftApplication;
+
+        this.RequestRewardBasedVideo();
+    }
+
+
+    private void RequestRewardBasedVideo()
+    {
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the rewarded video ad with the request.
+        this.rewardBasedVideoAd.LoadAd(request, rewardedVideoAd_ID);
+    }
+
+
+    public void ShowRewardVideoAd()
+    {
+        if (rewardBasedVideoAd.IsLoaded())
+        {
+            rewardBasedVideoAd.Show();
+        }
+    }
+
+
+    public void HandleRewardBasedVideoLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoLoaded event received");
+    }
+
+
+    public void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print(
+            "HandleRewardBasedVideoFailedToLoad event received with message: "
+                             + args.Message);
+    }
+
+
+    public void HandleRewardBasedVideoOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoOpened event received");
+    }
+
+
+    public void HandleRewardBasedVideoStarted(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoStarted event received");
+    }
+
+
+    public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoClosed event received");
+    }
+
+
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+    {
+        string type = args.Type;
+        double amount = args.Amount;
+        MonoBehaviour.print(
+            "HandleRewardBasedVideoRewarded event received for "
+                        + amount.ToString() + " " + type);
+    }
+
+
+    public void HandleRewardBasedVideoLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoLeftApplication event received");
+    }
+}
+
