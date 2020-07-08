bfd39dc646e67fc2002adf9bd5c071233b37ddab
AlexeyR1 <repin.alex2099@yandex.ru>
2020-07-08 23:06:38 +0300

    Added PlayerStatsDataModel

---
+++ Jonks/Assets/Scripts/Player/DataModel/PlayerStatsDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Assets.Scripts.Player.DataModel
+{
+    [Serializable]
+    public class PlayerStatsDataModel : Singleton<PlayerStatsDataModel>
+    {
+        // Лучшие результаты за все время игры
+        public int maxCollectedCoinsNumber;
+        public int maxEarnedPointsNumber;
+        public int maxPointsMultiplierValue;
+        public float maxLifeTime;
+        public float maxJumpHeight;
+
+        // Общие результаты за все время игры
+        public int totalCollectedCoinsNumber;
+        public TimeSpan totalGameTime;
+    }
+}
