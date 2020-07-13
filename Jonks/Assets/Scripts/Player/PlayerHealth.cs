27cba3db1f7cbeeb38e613623552f5a99a9e15d2
PurposeTo <firecrystal1999@gmail.com>
2020-07-14 00:21:04 +0300

    Update raiseTheDead

--- Jonks/Assets/Scripts/Player/PlayerHealth.cs
+++ Jonks/Assets/Scripts/Player/PlayerHealth.cs
@@ -7,7 +7,7 @@ public class PlayerHealth : MonoBehaviour
 
     private bool isInvulnerable = false;
 
-    private readonly float raiseImpulse = 60f;
+    private readonly float raiseHeight = 65f;
 
 
     private void Start()
@@ -25,7 +25,7 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
-    
+
     // Спасибо юнити аниматору, что он не видит методы, в которые необходимо передавать аргументы
     private void SetInvulnerableTrue()
     {
@@ -48,7 +48,14 @@ public class PlayerHealth : MonoBehaviour
         Vector2 toCentreDirection = ((Vector2)GameManager.Instance.Centre.transform.position - rb2D.position).normalized;
 
         rb2D.velocity = Vector2.zero;
-        rb2D.AddForce(-1 * toCentreDirection * raiseImpulse, ForceMode2D.Impulse);
+
+
+        float impulseVelocity = Mathf.Sqrt(raiseHeight * rb2D.mass * 2 * Gravity.GravityAcceleration * Gravity.GravityScale);
+        rb2D.AddForce(-1 * toCentreDirection * impulseVelocity, ForceMode2D.Impulse);
+
+        float fallingTime = Mathf.Sqrt(2 * raiseHeight / (Gravity.GravityAcceleration * Gravity.GravityScale));
+        Debug.Log($"Your fallingTime after raise is {fallingTime}");
+
     }
 
 
