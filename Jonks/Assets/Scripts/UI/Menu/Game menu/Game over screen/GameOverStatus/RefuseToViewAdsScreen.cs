3d944999ab403dcdf3385ea41a06e603a3cc81ad
PurposeTo <firecrystal1999@gmail.com>
2020-08-10 21:18:20 +0300

    Add RefuseToViewAdsScreen

---
+++ Jonks/Assets/Scripts/UI/Menu/Game menu/Game over screen/GameOverStatus/RefuseToViewAdsScreen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RefuseToViewAdsScreen : MonoBehaviour
+{
+    public void OpenMainMenu()
+    {
+        SceneLoader.LoadScene(SceneLoader.MainMenuName);
+    }
+
+
+    public void ReloadLvl()
+    {
+        SceneLoader.LoadScene(SceneLoader.GameSceneName);
+    }
+}
