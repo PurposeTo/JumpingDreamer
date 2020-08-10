955e3487521c904335f7a6e9c1fa81b1aa141a6f
PurposeTo <firecrystal1999@gmail.com>
2020-08-10 15:45:24 +0300

    Added ShowFinalScore class

---
+++ Jonks/Assets/Scripts/UI/Menu/Game menu/Game over screen/ShowFinalScore.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ShowFinalScore : MonoBehaviour
+{
+    public TextMeshProUGUI EarnedScore;
+
+
+    private void Awake()
+    {
+
+    }
+
+
+    private void OnDestroy()
+    {
+
+    }
+
+
+    private void ShowScore()
+    {
+        int score = GameManager.Instance.PlayerPresenter.ScoreCollector.Score;
+        string scoreText = $"Score\n{score}";
+
+        int stars = GameManager.Instance.PlayerPresenter.StarCollector.Stars;
+        string starsText = $"Stars\n{stars}";
+
+        EarnedScore.text = $"{scoreText}\n{starsText}";
+    }
+
+
+    private void ShowScoreWithRecord(bool isRecordNew)
+    {
+
+    }
+}
