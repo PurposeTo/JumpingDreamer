bd238c7e451d0ab173704fe887babfd2907e7d93
PurposeTo <firecrystal1999@gmail.com>
2020-08-30 03:27:55 +0300

    Update DataModel

--- Jonks/Assets/Scripts/Player Data/Model/PlayerInGamePurchases.cs
+++ Jonks/Assets/Scripts/Player Data/Model/PlayerInGamePurchases.cs
@@ -3,25 +3,29 @@
 public class PlayerInGamePurchases
 {
     [JsonConverter(typeof(SafeIntConverter))] public SafeInt? TotalCollectedStars { get; set; }
+    [JsonConverter(typeof(SafeIntConverter))] public SafeInt? CostValueIndexStars { get; set; } // Не может уменьшаться!
 
 
     public static PlayerInGamePurchases CreateInGamePurchasesWithDefaultValues()
     {
         return new PlayerInGamePurchases
         {
-            TotalCollectedStars = default(int)
+            TotalCollectedStars = default(int),
+            CostValueIndexStars = default(int)
         };
     }
 
 
     public void SaveTotalStarsData(SafeInt starsAmount)
     {
-        PlayerDataModelController.Instance.PlayerDataLocalModel.PlayerInGamePurchases.TotalCollectedStars += starsAmount;
+        TotalCollectedStars += starsAmount;
+        CostValueIndexStars += starsAmount;
     }
 
 
     public bool IsInGamePurchasesHaveNullValues()
     {
-        return !TotalCollectedStars.HasValue;
+        return !TotalCollectedStars.HasValue ||
+          !CostValueIndexStars.HasValue;
     }
 }
