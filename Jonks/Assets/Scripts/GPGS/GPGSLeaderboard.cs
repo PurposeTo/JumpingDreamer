2e89dd9f53b27f0ee56ae647b4ba924daf8680fa
AlexeyR1 <repin.alex2099@yandex.ru>
2020-09-22 22:46:39 +0300

    UpdateLeaderboard() method was implemented

--- Jonks/Assets/Scripts/GPGS/GPGSLeaderboard.cs
+++ Jonks/Assets/Scripts/GPGS/GPGSLeaderboard.cs
@@ -1,21 +1,49 @@
 ﻿using UnityEngine;
+using System;
 
 public class GPGSLeaderboard : SingletonMonoBehaviour<GPGSLeaderboard>
 {
-    public void OpenLeaderboard()
+    private Action UpdateLeaderboard;
+
+
+    private void Start()
     {
-        Social.ShowLeaderboardUI();
+        PlayerDataModelController.Instance.PlayerDataLocalModel.PlayerStats.OnNewScoreRecord += SetUpdateLeaderboardMethodToAction;
     }
 
 
-    public void UpdateLeaderboardScore()
+    private void OnDestroy()
     {
-        Social.ReportScore(PlayerDataModelController.Instance.PlayerDataLocalModel.PlayerStats.MaxEarnedScore.Value, GPGSIds.leaderboard_kings_of_the_jonks, (bool success) =>
+        PlayerDataModelController.Instance.PlayerDataLocalModel.PlayerStats.OnNewScoreRecord -= SetUpdateLeaderboardMethodToAction;
+    }
+
+
+    public void OpenLeaderboard()
+    {
+        UpdateLeaderboard?.Invoke();
+        if (UpdateLeaderboard == null) Social.ShowLeaderboardUI();
+    }
+
+
+    public void UpdateLeaderboardScore(Action openLeaderboardAction)
+    {
+        if (PlayerDataModelController.Instance.IsDataFileLoaded)
         {
-            if (success)
+            Social.ReportScore(PlayerDataModelController.Instance.PlayerDataLocalModel.PlayerStats.MaxEarnedScore.Value, GPGSIds.leaderboard_kings_of_the_jonks, (bool success) =>
             {
-                Debug.Log("The score is load to leaderboard!");
-            }
-        });
+                if (success)
+                {
+                    Debug.Log("The score is load to leaderboard!");
+                    openLeaderboardAction?.Invoke();
+                    UpdateLeaderboard = null;
+                }
+            });
+        }
+    }
+
+
+    private void SetUpdateLeaderboardMethodToAction()
+    {
+        UpdateLeaderboard = () => UpdateLeaderboardScore(() => Social.ShowLeaderboardUI());
     }
 }
