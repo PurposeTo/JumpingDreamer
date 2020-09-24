377a7018ce431c638ad719364617db823386626b
AlexeyR1 <repin.alex2099@yandex.ru>
2020-09-24 22:13:07 +0300

    Added Flash obstacle

---
+++ Jumping dreamer/Assets/Scripts/Obstacles/Flash.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flash : MonoBehaviour, IPooledObject
+{
+    //private readonly float width = 5f;
+    private readonly float lifetime = 4f;
+
+
+    private IEnumerator DestroyFlashEnumerator()
+    {
+        yield return new WaitForSeconds(lifetime);
+        gameObject.SetActive(false);
+    }
+
+
+    void IPooledObject.OnObjectSpawn()
+    {
+        StartCoroutine(DestroyFlashEnumerator());
+    }
+}
+
