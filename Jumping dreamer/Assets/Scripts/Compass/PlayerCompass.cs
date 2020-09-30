29ccd585599f4e9ce662c54063a3c862ed6c71a8
PurposeTo <firecrystal1999@gmail.com>
2020-09-30 20:05:31 +0300

    Create player compass simple

---
+++ Jumping dreamer/Assets/Scripts/Compass/PlayerCompass.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCompass : MonoBehaviour
+{
+    private GameObject player;
+    private SpriteRenderer sprite;
+
+    private float MinHighest => MaxHighest * 2f / 3f;
+    private float MaxHighest => PlatformGeneratorData.PlatformAvailableHighestArea;
+
+    private float alphaChanel;
+
+
+    private void Start()
+    {
+        player = GameManager.Instance.Player;
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+
+    private void Update()
+    {
+        float currentRange = GameManager.Instance.GetToCentreMagnitude(player.transform.position);
+        currentRange -= MinHighest;
+
+        alphaChanel = Mathf.Clamp(currentRange / MaxHighest, 0, 1);
+
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3f, player.transform.position.z);
+        sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, alphaChanel);
+        print($"AlphaChanel: {alphaChanel}");
+    }
+}
