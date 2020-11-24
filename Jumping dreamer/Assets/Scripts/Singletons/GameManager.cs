156ace211c54f8d57cb56f6b7f7c2f536cd79e02
PurposeTo <firecrystal1999@gmail.com>
2020-11-24 19:41:32 +0300

    Update GameManager.cs

--- Jumping dreamer/Assets/Scripts/Singletons/GameManager.cs
+++ Jumping dreamer/Assets/Scripts/Singletons/GameManager.cs
@@ -6,9 +6,12 @@ public class GameManager : SingletonSuperMonoBehaviour<GameManager>
     public event Action OnGameOver;
 
 
-    private bool isPause = false;
-    private bool isGameReady = false;
-
+    private bool isGameReady = false; // Игра готова? (Используется загрузчиком сцен)
+    private bool isWaiting = false; // Нужно подождать? (Используется индикатором загрузки)
+    // Todo: реализовать использование.
+    private bool isGameActive = false; // Игра активна? (Включается при загрузки игры/возрождении, выключается при смерти)
+    private bool isPause = false; // Игра на паузе? (Переключается игроком через меню паузы)
+    
 
     public void GameOver()
     {
@@ -16,24 +19,37 @@ public class GameManager : SingletonSuperMonoBehaviour<GameManager>
         OnGameOver?.Invoke();
     }
 
-
+    /// <summary>
+    /// Установить паузу игры
+    /// </summary>
     public void SetPause(bool isPause)
     {
         this.isPause = isPause;
         SetTimeScale();
     }
 
-
+    /// <summary>
+    /// Установить значение готовности приложения. Используется загрузчиком сцен.
+    /// </summary>
     public void SetGameReady(bool isGameReady)
     {
         this.isGameReady = isGameReady;
         SetTimeScale();
     }
 
+    /// <summary>
+    /// Установить значение ожидания
+    /// </summary>
+    public void SetWaiting(bool isWaiting)
+    {
+        this.isWaiting = isWaiting;
+        SetTimeScale();
+    }
+
 
     private void SetTimeScale()
     {
-        if (isPause || !isGameReady) Time.timeScale = 0f;
+        if (isPause || !isGameReady || isWaiting) Time.timeScale = 0f;
         else Time.timeScale = 1f;
     }
 }
