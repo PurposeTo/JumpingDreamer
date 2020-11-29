f7aad2c306b89eaa0016c4b7c7e58c17fd142685
PurposeTo <firecrystal1999@gmail.com>
2020-11-29 21:43:12 +0300

    Update player view

--- Jumping dreamer/Assets/Scripts/Player/PlayerPresenter.cs
+++ Jumping dreamer/Assets/Scripts/Player/PlayerPresenter.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 
-public class PlayerPresenter : MonoBehaviour
+public class PlayerPresenter : SuperMonoBehaviour
 {
     public Rigidbody2D PlayerRigidbody2D { get; private set; }
 
     public PlayerHealth PlayerHealth { get; private set; }
     public PlayerMovement PlayerMovement { get; private set; }
+    public PlayerView PlayerView { get; private set; }
+
+
     public ScoreCollector ScoreCollector { get; private set; }
     public StarCollector StarCollector { get; private set; }
     public SpeedTrail SpeedTrail { get; private set; }
@@ -14,10 +17,11 @@ public class PlayerPresenter : MonoBehaviour
     public GravitySensitive GravitySensitive { get; private set; }
 
 
-    private void Awake()
+    protected override void AwakeWrapped()
     {
         PlayerRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
         PlayerHealth = gameObject.GetComponent<PlayerHealth>();
+        PlayerView = gameObject.GetComponent<PlayerView>();
         PlayerMovement = gameObject.GetComponent<PlayerMovement>();
         ScoreCollector = gameObject.GetComponent<ScoreCollector>();
         StarCollector = gameObject.GetComponent<StarCollector>();
